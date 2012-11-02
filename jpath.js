229a0db0b1993abe8a04c6548a3874818ca25ef5
Sam Tsvilik <webdevsimon@gmail.com>
2012-11-02 11:06:07 -0400

    jpath.js

--- jpath.js
+++ jpath.js
@@ -10,9 +10,9 @@
 		FLAGS = 'g',
 		NULL = null,
 		
-		rxTokens = new RegExp("([A-Za-z0-9_\\*@\\$]+(?:\\[.+?\\])?)", FLAGS),
-		rxIndex = new RegExp("(\\S+)\\[(\\d+)\\]"),
-		rxPairs = new RegExp("([\\w@\\.]+)\\s*([~><\\^\\*\\$\\!=]=?|\\?)\\s*([@\\w\\s_\\'\$\\.\\+-]+)(\\s*|$)", FLAGS),
+        rxTokens = new RegExp("([A-Za-z0-9_\\*@\\$\\(\\)]+(?:\\[.+?\\])?)", "g"),
+        rxIndex = /^(\S+)\((\d+)\)$/,
+		rxPairs = new RegExp("([\\w@\\.\\(\\)]+)\\s*([~><\\^\\*\\$\\!=]=?|\\?)\\s*([\\S ]+)(\\s*|$)", "g"),
 		rxCondition = new RegExp("(\\S+)\\[(.+)\\]"),
 		trimBefore = new RegExp("^\\s\\s*"),
 		trimAfter = new RegExp("\\s\\s*$"),
@@ -27,49 +27,53 @@
 			toArray: function(o) {
 				return o instanceof Array? o : (o === undef || o === NULL) ? []:[o];
 			},
-			traverse: function(pattern, cfn, obj){
-				var out, data = (obj || this.data), temp, tokens, token, idxToken, index, expToken, condition, tail, self = arguments.callee, found, i, j, l;
-				if(data && typeof(pattern) === STRING) {
+			traverse: function(pattern, cfn, obj) {
+				var out, data = (obj || this.data),
+					temp, tokens, token, idxToken, index, expToken, condition, tail, self = arguments.callee,
+					found, i, j, l;
+				if (data && typeof(pattern) === STRING) {
 					tokens = pattern.match(rxTokens); //dot notation splitter
 					//Get first token
 					token = tokens[0];
 					//Trailing tokens
 					tail = tokens.slice(1).join(PERIOD);
-					
-					if( data instanceof Array ) {
+                    
+					if (data instanceof Array) {
 						temp = [];
-						for(i = 0, l = data.length; l > i; i++) {
+						for (i = 0, l = data.length; l > i; i++) {
 							j = data[i];
-							found = self.apply(this, [token, NULL, j]);
-							if(((found instanceof Array) && found.length) || found !== undef) {
+							found = self.apply(this, [token, cfn, j]);
+							if (((found instanceof Array) && found.length) || found !== undef) {
 								app.apply(temp, [found]);
 							}
 						}
-						if(temp.length) {
-							return tail ? self.apply(this, [tail, NULL, temp]) : temp;
+						if (temp.length) {
+							return tail ? self.apply(this, [tail, cfn, temp]) : temp;
 						} else {
-							return;	
+							return;
 						}
-					} else if(token === "*") {
-						return tail ? self.apply(this, [tail, NULL, data]) : data;
-					} else if(data[token] !== undef) {
-						return tail ? self.apply(this, [tail, NULL, data[token]]) : data[token];
-					} else if( rxIndex.test(token) ) {
+					} else if (token === "*") {
+						return tail ? self.apply(this, [tail, cfn, data]) : data;
+					} else if (data[token] !== undef) {
+						return tail ? self.apply(this, [tail, cfn, data[token]]) : data[token];
+					} else if (rxIndex.test(token)) {
 						idxToken = token.match(rxIndex);
 						token = idxToken[1];
 						index = +idxToken[2];
-						return tail ? self.apply(this, [tail, NULL, data[token][index]]) : data[token][index];
-					} else if( rxCondition.test(token) ) {
+						temp = data[token];
+						return tail ? self.apply(this, [tail, cfn, (temp && temp.length) ? temp[index] : temp]) : (temp && temp.length) ? temp[index] : temp;
+					} else if (rxCondition.test(token)) {
 						expToken = token.match(rxCondition);
 						token = expToken[1];
 						condition = expToken[2];
-						
-						var evalStr, isMatch, subset = data[token], elem;
-						
-						if(subset instanceof Array) {
+                        
+						var evalStr, isMatch, subset = token === "*" ? data : data[token],
+							elem;
+                            
+						if (subset instanceof Array) {
 							temp = [];
 							//Second loop here is faster than recursive call
-							for(i = 0, l = subset.length; l > i; i++) {
+							for (i = 0, l = subset.length; l > i; i++) {
 								elem = subset[i];
 								//Convert condition pairs to booleans
 								evalStr = condition.replace(rxPairs, function($0, $1, $2, $3) {
@@ -77,25 +81,25 @@
 								});
 								//Evaluate expression
 								isMatch = eval(evalStr);
-								if(isMatch) {
+								if (isMatch) {
 									app.apply(temp, [elem]);
 								}
 							}
-							if(temp.length) {
-								return tail ? self.apply(this, [tail, NULL, temp]) : temp;
+							if (temp.length) {
+								return tail ? self.apply(this, [tail, cfn, temp]) : temp;
 							} else {
-								return;	
+								return;
 							}
 						} else {
-							elem = data;
+							elem = subset;
 							//Convert condition pairs to booleans
 							evalStr = condition.replace(rxPairs, function($0, $1, $2, $3) {
 								return hidden.testPairs.apply(elem, [$1, $3, $2, cfn]);
 							});
 							//Evaluate expression
 							isMatch = eval(evalStr);
-							if(isMatch) {
-								return tail ? self.apply(this, [tail, NULL, elem]) : elem;
+							if (isMatch) {
+								return tail ? self.apply(this, [tail, cfn, elem]) : elem;
 							}
 						}
 					}
@@ -105,39 +109,88 @@
 			//Matches type of a to b
 			matchTypes: function(a, b) {
 				var _a, _b;
-				switch(typeof(a)) {
-					case STRING: _b = b+''; break;
-					case "boolean": _b = b === "true"?TRUE:FALSE; break;
-					case "number": _b = +b; break;
-					case "date": _b = new Date(b).valueOf(); _a = a.valueOf(); break;
-					default: _b = b;
+				switch (typeof(a)) {
+				case "string":
+					_b = b + '';
+					break;
+				case "boolean":
+					_b = b === "true" ? TRUE : FALSE;
+					break;
+				case "number":
+					_b = +b;
+					break;
+				case "date":
+					_b = new Date(b).valueOf();
+					_a = a.valueOf();
+					break;
+				default:
+					_b = b;
 				}
-				if(b === "null") { _b = NULL; }
-				if(b === "undefined") { _b = void(0); }
-				return {left:(_a||a), right:_b};
-			},
-			testPairs: function(left, right, operator, fn) {
-				var out = FALSE, 
-					leftVal = left.indexOf(PERIOD) !== -1 ? hidden.traverse(left, NULL, this) : this[left],
-					pairs = hidden.matchTypes(leftVal, hidden.trim(right));
-				switch(operator) {
-					case "=": out = (pairs.left === pairs.right); break;
-					case "==": out = (pairs.left === pairs.right); break;
-					case "!=": out = (pairs.left !== pairs.right); break;
-					case "^=": out = (pairs.left+'').indexOf(pairs.right) === 0; break;
-					case "<": out = (pairs.left < pairs.right); break;
-					case "<=": out = (pairs.left <= pairs.right); break;
-					case ">": out = (pairs.left > pairs.right); break;
-					case ">=": out = (pairs.left >= pairs.right); break;
-					case "~=": out = ((pairs.left+'').toLowerCase() === (pairs.right+'').toLowerCase()); break;
-					case "$=": out = new RegExp(pairs.right+"$", "i").test(pairs.left); break;
-					case "*=": out = (pairs.left+'').indexOf(pairs.right)!==-1; break;
-					case "?": if(typeof(fn) === FUNCTION) { out = fn.call(this, left, right); } break;
+				if (b === "null") {
+					_b = null;
 				}
-				return out;
+				if (b === "undefined") {
+					_b = void(0);
+				}
+				return {
+					left: (_a || a),
+					right: _b
+				};
 			},
+			testPairs: (function() {
+				var conditions = {
+					"=": function(l, r) {
+						return l === r;
+					},
+					"==": function(l, r) {
+						return l === r;
+					},
+					"!=": function(l, r) {
+						return l !== r;
+					},
+					"^=": function(l, r) {
+						return !((l + '').indexOf(r));
+					},
+					"<": function(l, r) {
+						return l < r;
+					},
+					"<=": function(l, r) {
+						return l <= r;
+					},
+					">": function(l, r) {
+						return l > r;
+					},
+					">=": function(l, r) {
+						return l >= r;
+					},
+					"~=": function(l, r) {
+						return (l + '').toLowerCase() === (r + '').toLowerCase();
+					},
+					"$=": function(l, r) {
+						return new RegExp(r + "$", "i").test(l);
+					},
+					"*=": function(l, r) {
+						return (l + '').indexOf(r) >= 0;
+					}
+				};
+                
+				return function(left, right, operator, fn) {
+					var out = FALSE,
+						leftVal = left.indexOf(PERIOD) !== -1 ? hidden.traverse(left, null, this) : this[left],
+						pairs = hidden.matchTypes(leftVal, hidden.trim(right));
+					if (operator === "?") {
+						if (typeof(fn) === FUNCTION) {
+							out = fn.call(this, pairs.left, right);
+						}
+					} else {
+						out = conditions[operator](pairs.left, pairs.right);
+					}
+					return out;
+				};
+			})(),
 			merge: function(pattern) {
-				var out = [], temp = hidden.toArray(pattern ? hidden.traverse.apply(this, arguments) : this.selection);
+				var out = [],
+					temp = hidden.toArray(pattern ? hidden.traverse.apply(this, arguments) : this.selection);
 				out = apc.apply([], temp);
 				return out;
 			}
