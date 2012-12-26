433992a2a29cb016064acbd6920d595f39541ac6
Simon Tsvilik <sam@Simons-MacBook-Pro.local>
2012-12-26 12:57:46 -0500

    Conditional pattern updat
    RegEx that processes conditions has been greatly improved to cover more
    test cases and fix some old errors.
    
    - Minor performance optimizations
    - Allowing escaped quotes in conditions
    - Logic grouping using parenteces now works properly
    
    Possible breaking changes:
    - Evaluating against a value that contains spaces now REQUIRES quotes
    around it.
    - *= operation now ignores case
    
    Unit Testing:
    QUnit test page is now included. Use it to see all possible test/use
    scenarios and introduce improvements.

--- README.md
+++ README.md
@@ -2,7 +2,7 @@ Can't get deeper than jPath does
 ---
 Well folks, JPath came about the same time I got pissed-off at heavily nested JSON objects.
 It was both lengthy and unreadable to write a whole line of "If" checks just to get data
-nested 4 levels deep inside a JSON object. 
+nested 4 levels deep inside a JSON object.
 
 JPath is a traverse tool that allows XPATH-like navigation within a
 JSON structure and a little more. JPath also uses pattern syntax more familiar to JavaScript developers in the sense
@@ -19,7 +19,7 @@ Just like XPATH, it does support conditional filtering, where you basically spec
 based on certain condition. Conditional queries work by comparing data members to value you provide inside your
 expression (it does not do comparing between data members). So for example if you have an array of objects and you want
 to get only those objects where member foo = 1, you would write "obj[foo == 1]", more examples later. It supports a
-wide range of evaluations here is the list:
+wide range of evaluations.
 
 - "==" | "=" - compares data member for equality
 - "!=" - compares data member inequality
@@ -30,15 +30,15 @@ wide range of evaluations here is the list:
 - "~=" - equal ignoring case
 - "^=" - starts with
 - "$=" - ends with
-- "*=" - contains a string anywhere inside
-- "?" - allows you to pass a custom evaluation function
+- "*=" - contains a string anywhere inside (case insensitive)
+- "?" - allows you to pass a custom evaluation function (runs in the scope of evaluated object so you can compare against other object members).
 
-During the comparing stage, all values are type matched (coerced) to the types of values you're comparing against. 
+During the comparing stage, all values are type matched (coerced) to the types of values you're comparing against.
 What this means is that you always compare numbers against numbers and not strings, and same goes for every other data
 type.
 
-If your value contains a space, you can enclose your value in single quotes. (i.e. [foo == 'hello world']) Normally you
-don't have to do that.
+If your value contains a space, you can enclose your value in single or double quotes. (i.e. [foo == 'hello world']) Normally you
+don't have to do that. If youre value contains quotes, you can escape them using double slashes (i.e [foo == 'Bob\\\'s']).
 
 #### What else can it do?
 
@@ -68,13 +68,13 @@ JPath
 select( json, expression [,cust_compare_fn] ) - performs a traversal and returns you an instance of JPath object
 filter( json, expression [,cust_compare_fn] ) - performs a traversal and returns a value
 
-#### How to's
+#### Examples
 
 1. Using Custom compare logic
 
         jPath.filter( JSON, "foo[bar ? test]", function(left, right) {
             //left - is the value of the data member bar inside foo
-            //right - woudl be equal to "test"
+            //right - would be equal to "test"
             return left + "blah" == right; //Cusom validation
         });
 
@@ -93,3 +93,10 @@ filter( json, expression [,cust_compare_fn] ) - performs a traversal and returns
 4. Accessing array elements by index
 
         jPath.select({myArray:[1,2,3,4,5]}, "myArray(0)");
+
+5. Using parenteces to group logic
+        //Using logical groups is valid and evaluated properly
+        jPath.filter(obj, "*[(a==b || a == c) && c == d]");
+
+#### Unit Test
+Please find included test.html - it's a QUnit test to validate most of the possible test scenarios.
--- jpath.js
+++ jpath.js
@@ -1,105 +1,109 @@
 //jpath.js - is a library that allows filtering of JSON data based on pattern-like expression
-(function(Array, undef) {	
-	var 
-		TRUE = !0,
+(function(Array, undef) {
+	var
+	TRUE = !0,
 		FALSE = !1,
 		STRING = "string",
 		FUNCTION = "function",
 		PERIOD = ".",
-		SPACE = '',
+		EMPTY = '',
 		FLAGS = 'g',
 		NULL = null,
-		
-        rxTokens = new RegExp("([A-Za-z0-9_\\*@\\$\\(\\)]+(?:\\[.+?\\])?)", "g"),
-        rxIndex = /^(\S+)\((\d+)\)$/,
-		rxPairs = new RegExp("([\\w@\\.\\(\\)]+)\\s*([~><\\^\\*\\$\\!=]=?|\\?)\\s*([\\S ]+)(\\s*|$)", "g"),
-		rxCondition = new RegExp("(\\S+)\\[(.+)\\]"),
-		trimBefore = new RegExp("^\\s\\s*"),
-		trimAfter = new RegExp("\\s\\s*$"),
-		
+
+		rxTokens = /([A-Za-z0-9_\*@\$\(\)]+(?:\[.+?\])?)/g,
+		rxIndex = /^(\S+)\((\d+)\)$/,
+		rxPairs = /(\(+)?([\w\.\(\)\$\_]+)(?:\s*)([\=\^\!\*\~\>\<\?\$]{1,2})\s*(?:\s*)("(?:[^"\\]|\\.)*"|'(?:[^'\\]|\\.)*'|[^' \&\|\)\(]+)\s*(\)+)?/g,
+		rxCondition = /(\S+)\[(.+)\]/,
+		rxTrim = /\S+(?:\s+\S+)*/,
+		rxEscQuote = /\\('|")/g,
+
 		app = Array.prototype.push,
 		apc = Array.prototype.concat,
-		
+
 		hidden = {
-			trim: function( s ) {
-				return s.replace(trimBefore, SPACE).replace(trimAfter, SPACE);
+			trim: function(s) {
+				var str = s.match(rxTrim);
+				return str ? str[0] : EMPTY;
+			},
+			qtrim: function(s) {
+				return((!s.indexOf("'") || !s.indexOf('"')) && (s.slice(-1) === "'" || s.slice(-1) === '"')) ? s.slice(1, -1) : s;
 			},
 			toArray: function(o) {
-				return o instanceof Array? o : (o === undef || o === NULL) ? []:[o];
+				return o instanceof Array ? o : (o === undef || o === NULL) ? [] : [o];
 			},
 			traverse: function(pattern, cfn, obj) {
 				var out, data = (obj || this.data),
 					temp, tokens, token, idxToken, index, expToken, condition, tail, self = arguments.callee,
 					found, i, j, l;
-				if (data && typeof(pattern) === STRING) {
+				if(data && typeof(pattern) === STRING) {
 					tokens = pattern.match(rxTokens); //dot notation splitter
 					//Get first token
 					token = tokens[0];
 					//Trailing tokens
 					tail = tokens.slice(1).join(PERIOD);
-                    
-					if (data instanceof Array) {
+
+					if(data instanceof Array) {
 						temp = [];
-						for (i = 0, l = data.length; l > i; i++) {
-							j = data[i];
-							found = self.apply(this, [token, cfn, j]);
-							if (((found instanceof Array) && found.length) || found !== undef) {
-								app.apply(temp, [found]);
+						for(i = 0, j;
+						(j = data[i]) != NULL; i++) {
+							found = self.call(this, token, cfn, j);
+							if(((found instanceof Array) && found.length) || found !== undef) {
+								app.call(temp, found);
 							}
 						}
-						if (temp.length) {
-							return tail ? self.apply(this, [tail, cfn, temp]) : temp;
+						if(temp.length) {
+							return tail ? self.call(this, tail, cfn, temp) : temp;
 						} else {
 							return;
 						}
-					} else if (token === "*") {
-						return tail ? self.apply(this, [tail, cfn, data]) : data;
-					} else if (data[token] !== undef) {
-						return tail ? self.apply(this, [tail, cfn, data[token]]) : data[token];
-					} else if (rxIndex.test(token)) {
+					} else if(token === "*") {
+						return tail ? self.call(this, tail, cfn, data) : data;
+					} else if(data[token] !== undef) {
+						return tail ? self.call(this, tail, cfn, data[token]) : data[token];
+					} else if(rxIndex.test(token)) {
 						idxToken = token.match(rxIndex);
 						token = idxToken[1];
 						index = +idxToken[2];
 						temp = data[token];
-						return tail ? self.apply(this, [tail, cfn, (temp && temp.length) ? temp[index] : temp]) : (temp && temp.length) ? temp[index] : temp;
-					} else if (rxCondition.test(token)) {
+						return tail ? self.call(this, tail, cfn, (temp && temp.length) ? temp[index] : temp) : (temp && temp.length) ? temp[index] : temp;
+					} else if(rxCondition.test(token)) {
 						expToken = token.match(rxCondition);
 						token = expToken[1];
 						condition = expToken[2];
-                        
+
 						var evalStr, isMatch, subset = token === "*" ? data : data[token],
 							elem;
-                            
-						if (subset instanceof Array) {
+
+						if(subset instanceof Array) {
 							temp = [];
 							//Second loop here is faster than recursive call
-							for (i = 0, l = subset.length; l > i; i++) {
-								elem = subset[i];
+							for(i = 0;
+							(elem = subset[i]) != NULL; i++) {
 								//Convert condition pairs to booleans
-								evalStr = condition.replace(rxPairs, function($0, $1, $2, $3) {
-									return hidden.testPairs.apply(elem, [$1, $3, $2, cfn]);
+								evalStr = condition.replace(rxPairs, function(match, pl, left, operator, right, pr) {
+									return [pl, hidden.testPairs.call(elem, left, right, operator, cfn), pr].join(EMPTY);
 								});
 								//Evaluate expression
 								isMatch = eval(evalStr);
-								if (isMatch) {
-									app.apply(temp, [elem]);
+								if(isMatch) {
+									app.call(temp, elem);
 								}
 							}
-							if (temp.length) {
-								return tail ? self.apply(this, [tail, cfn, temp]) : temp;
+							if(temp.length) {
+								return tail ? self.call(this, tail, cfn, temp) : temp;
 							} else {
 								return;
 							}
 						} else {
 							elem = subset;
 							//Convert condition pairs to booleans
-							evalStr = condition.replace(rxPairs, function($0, $1, $2, $3) {
-								return hidden.testPairs.apply(elem, [$1, $3, $2, cfn]);
+							evalStr = condition.replace(rxPairs, function(match, pl, left, operator, right, pr) {
+								return [pl, hidden.testPairs.call(elem, left, right, operator, cfn), pr].join(EMPTY);
 							});
 							//Evaluate expression
 							isMatch = eval(evalStr);
-							if (isMatch) {
-								return tail ? self.apply(this, [tail, cfn, elem]) : elem;
+							if(isMatch) {
+								return tail ? self.call(this, tail, cfn, elem) : elem;
 							}
 						}
 					}
@@ -109,9 +113,9 @@
 			//Matches type of a to b
 			matchTypes: function(a, b) {
 				var _a, _b;
-				switch (typeof(a)) {
+				switch(typeof(a)) {
 				case "string":
-					_b = b + '';
+					_b = b + EMPTY;
 					break;
 				case "boolean":
 					_b = b === "true" ? TRUE : FALSE;
@@ -126,11 +130,11 @@
 				default:
 					_b = b;
 				}
-				if (b === "null") {
+				if(b === "null") {
 					_b = null;
 				}
-				if (b === "undefined") {
-					_b = void(0);
+				if(b === "undefined") {
+					_b = undef;
 				}
 				return {
 					left: (_a || a),
@@ -148,9 +152,6 @@
 					"!=": function(l, r) {
 						return l !== r;
 					},
-					"^=": function(l, r) {
-						return !((l + '').indexOf(r));
-					},
 					"<": function(l, r) {
 						return l < r;
 					},
@@ -164,22 +165,26 @@
 						return l >= r;
 					},
 					"~=": function(l, r) {
-						return (l + '').toLowerCase() === (r + '').toLowerCase();
+						return(l + EMPTY).toLowerCase() === (r + EMPTY).toLowerCase();
+					},
+					"^=": function(l, r) {
+						return !((l + EMPTY).indexOf(r));
 					},
 					"$=": function(l, r) {
-						return new RegExp(r + "$", "i").test(l);
+						return(r + EMPTY) === (l + EMPTY).slice(-(r + EMPTY).length);
 					},
 					"*=": function(l, r) {
-						return (l + '').indexOf(r) >= 0;
+						return(l + EMPTY).toLowerCase().indexOf((r + EMPTY).toLowerCase()) !== -1;
 					}
 				};
-                
+
 				return function(left, right, operator, fn) {
 					var out = FALSE,
-						leftVal = left.indexOf(PERIOD) !== -1 ? hidden.traverse(left, null, this) : this[left],
-						pairs = hidden.matchTypes(leftVal, hidden.trim(right));
-					if (operator === "?") {
-						if (typeof(fn) === FUNCTION) {
+						leftVal = left.indexOf(PERIOD) >= 0 ? hidden.traverse(left, null, this) : this[left],
+						//We clean up r to remove wrapping quotes and escaped quotes (both single/dbl)
+						pairs = hidden.matchTypes(leftVal, hidden.trim(hidden.qtrim(right)).replace(rxEscQuote,'$1'));
+					if(operator === "?") {
+						if(typeof(fn) === FUNCTION) {
 							out = fn.call(this, pairs.left, right);
 						}
 					} else {
@@ -195,18 +200,18 @@
 				return out;
 			}
 		},
-	
-	module = {};
-	
-	
+
+		module = {};
+
+
 	function JPath(obj) {
 		if(!(this instanceof JPath)) {
-			return new JPath(obj);	
+			return new JPath(obj);
 		}
 		this.data = obj || NULL;
 		this.selection = [];
 	}
-	
+
 	JPath.prototype = {
 		//Sets JSON object source on the fly
 		from: function(obj) {
@@ -214,19 +219,19 @@
 			return this;
 		},
 		//Returns the first element value of the search
-		first: function(){
-			return this.selection.length ? this.selection[0] : NULL;		
+		first: function() {
+			return this.selection.length ? this.selection[0] : NULL;
 		},
 		//Returns the last element value of the search
-		last:function(){
-			return this.selection.length ? this.selection.slice(-1)[0] : NULL;	
+		last: function() {
+			return this.selection.length ? this.selection.slice(-1)[0] : NULL;
 		},
 		//Returns an exact element value specified by index position
 		eq: function(idx) {
-			return this.selection.length ? this.selection[idx] : NULL;	
+			return this.selection.length ? this.selection[idx] : NULL;
 		},
 		//Performs a search on the JSON object
-		select:function(pattern, cfn, obj){
+		select: function(pattern, cfn, obj) {
 			this.selection = hidden.merge.apply(this, arguments);
 			return this;
 		},
@@ -237,10 +242,10 @@
 		},
 		//Returns search results as an Array
 		val: function() {
-			return this.selection;	
+			return this.selection;
 		}
 	};
-	
+
 	//Returns an insance if JPath object
 	//@obj - JSON object
 	//@pattern - <String> search pattern
@@ -255,7 +260,7 @@
 	//@pattern - <String> search pattern
 	//@cfn - <Function> custom compare function. This function has two input arguments: @left, @right
 	module.filter = function(obj, pattern, cfn) {
-		return JPath(obj).select(pattern, NULL, cfn).val();
+		return JPath(obj).select(pattern, cfn, NULL).val();
 	};
 
 	window.jPath = window.jPath || module;
